7c8fb3815964661ec4d2a23e49124b3f54dfe620
Ned Grady <gradyned@gmail.com>
2020-10-09 20:58:07 +0100

    Updates

---
+++ precache-manifest.3ca48f7d73e6a9828bdc4dafa469fc99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf7e9bb0fa3a50640fac505e2bdcb62",
+    "url": "/nedgradydotcom/index.html"
+  },
+  {
+    "revision": "2afffcb47c3a6fba7b1e",
+    "url": "/nedgradydotcom/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7af37ca7f9ebfabddbad",
+    "url": "/nedgradydotcom/static/js/2.f1bdf543.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nedgradydotcom/static/js/2.f1bdf543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2afffcb47c3a6fba7b1e",
+    "url": "/nedgradydotcom/static/js/main.83d18077.chunk.js"
+  },
+  {
+    "revision": "7c5e48bed95eebbe8d39",
+    "url": "/nedgradydotcom/static/js/runtime-main.b897d72c.js"
+  }
+]);
